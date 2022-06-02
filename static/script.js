5344d568d010adcd7399b9a83f8b00e12cc40d2c
julia8024 <julia8024@naver.com>
2022-06-02 21:21:27 +0900

    add

---
+++ static/script.js
@@ -0,0 +1,8 @@
+function loadURL(str) {
+    var url;
+    if (str == "insta") {
+        url = "https://www.instagram.com/julia_jang_illust/";
+    } else if (str == "github") {
+        url = ""
+    }
+}
